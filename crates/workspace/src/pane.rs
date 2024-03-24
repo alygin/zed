fbaf9ef9c537ff947b7cb40d94da6a811be90a2c
Andrew Lygin <alygin@gmail.com>
2024-03-24 19:09:03 +0300

    Tab Bar settings

--- crates/workspace/src/pane.rs
+++ crates/workspace/src/pane.rs
@@ -1,5 +1,5 @@
 use crate::{
-    item::{ClosePosition, Item, ItemHandle, ItemSettings, WeakItemHandle},
+    item::{ClosePosition, Item, ItemHandle, TabBarPlacement, TabsSettings, WeakItemHandle},
     toolbar::Toolbar,
     workspace_settings::{AutosaveSetting, WorkspaceSettings},
     NewCenterTerminal, NewFile, NewSearch, OpenVisible, SplitDirection, ToggleZoom, Workspace,
@@ -1303,19 +1303,31 @@ impl Pane {
         ix: usize,
         item: &Box<dyn ItemHandle>,
         detail: usize,
+        tab_bar_placement: ui::TabBarPlacement,
         cx: &mut ViewContext<'_, Pane>,
     ) -> impl IntoElement {
         let is_active = ix == self.active_item_index;
 
         let label = item.tab_content(Some(detail), is_active, cx);
-        let close_side = &ItemSettings::get_global(cx).close_position;
-        let indicator = render_item_indicator(item.boxed_clone(), cx);
+        let close_side = &TabsSettings::get_global(cx).close_position;
+
+        let indicator = maybe!({
+            let indicator_color = match (item.has_conflict(cx), item.is_dirty(cx)) {
+                (true, _) => Color::Warning,
+                (_, true) => Color::Accent,
+                (false, false) => return None,
+            };
+
+            Some(Indicator::dot().color(indicator_color))
+        });
+
         let item_id = item.item_id();
         let is_first_item = ix == 0;
         let is_last_item = ix == self.items.len() - 1;
         let position_relative_to_active_item = ix.cmp(&self.active_item_index);
 
         let tab = Tab::new(ix)
+            .tab_bar_placement(tab_bar_placement)
             .position(if is_first_item {
                 TabPosition::First
             } else if is_last_item {
@@ -1480,8 +1492,20 @@ impl Pane {
         })
     }
 
-    fn render_tab_bar(&mut self, cx: &mut ViewContext<'_, Pane>) -> impl IntoElement {
+    fn need_tab_bar_at(&self, placement: TabBarPlacement, cx: &mut ViewContext<'_, Pane>) -> bool {
+        let Some(item) = self.active_item() else {
+            return false;
+        };
+        item.tab_bar_placement(cx) == placement
+    }
+
+    fn render_tab_bar(
+        &mut self,
+        placement: ui::TabBarPlacement,
+        cx: &mut ViewContext<'_, Pane>,
+    ) -> impl IntoElement {
         TabBar::new("tab_bar")
+            .placement(placement)
             .track_scroll(self.tab_bar_scroll_handle.clone())
             .when(self.display_nav_history_buttons, |tab_bar| {
                 tab_bar.start_child(
@@ -1520,7 +1544,7 @@ impl Pane {
                     .iter()
                     .enumerate()
                     .zip(tab_details(&self.items, cx))
-                    .map(|((ix, item), detail)| self.render_tab(ix, item, detail, cx)),
+                    .map(|((ix, item), detail)| self.render_tab(ix, item, detail, placement, cx)),
             )
             .child(
                 div()
@@ -1814,8 +1838,8 @@ impl Render for Pane {
                     }
                 }),
             )
-            .when(self.active_item().is_some(), |pane| {
-                pane.child(self.render_tab_bar(cx))
+            .when(self.need_tab_bar_at(TabBarPlacement::Top, cx), |pane| {
+                pane.child(self.render_tab_bar(ui::TabBarPlacement::Top, cx))
             })
             .child({
                 let has_worktrees = self.project.read(cx).worktrees().next().is_some();
@@ -1882,6 +1906,9 @@ impl Render for Pane {
                                 }
                             }),
                     )
+            })
+            .when(self.need_tab_bar_at(TabBarPlacement::Bottom, cx), |pane| {
+                pane.child(self.render_tab_bar(ui::TabBarPlacement::Bottom, cx))
             })
             .on_mouse_down(
                 MouseButton::Navigate(NavigationDirection::Back),
